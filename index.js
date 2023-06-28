4736ab89a11777fe6964eca5d13bb5bb4623d556
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-28 11:35:41 +0100

    configured the mongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log('Database connected');
+})
 const app = express();
 
 app.listen(3000, () => {
